ed17a2540c43a4b3c6335a9087a03869fa5a7f0c
Rubal <rubal@bansal.in>
2021-03-19 16:34:36 +0530

    Registeration

--- project/notelog/views.py
+++ project/notelog/views.py
@@ -1,6 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import logout, authenticate
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
+
+
 def home(request):
     return HttpResponse("This is Home page")
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+@csrf_exempt
+def handleRegister(request):
+    if request.method == 'POST':
+        # Get the post parameters
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        # Check for errors
+        if not username.isalnum():
+            messages.error(
+                request, "Username must be only letters and numbers.")
+            return redirect('register')
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match.")
+            return redirect('register')
+
+        if len(password1) < 6:
+            messages.error(request, "Password should be greater than 6.")
+            return redirect('register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Already Registered. Try logging in")
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Already Registered. Try logging in")
+            return redirect('register')
+
+        # Create the user
+        myuser = User.objects.create_user(username, email, password1)
+        myuser.save()
+        messages.success(request, "Successfully, Registered.")
+        return redirect('register')
+    else:
+        return HttpResponse('404 - Not Found')
