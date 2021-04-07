d0ae9618761d569dfff4614e7ec3a0c88baa635d
sv-shivansh <sv_shivansh@outlook.com>
2021-04-07 21:09:52 +0530

    fixing

--- project/notelog/models.py
+++ project/notelog/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class toDo(models.Model):
     user = models.CharField(max_length=50)
     task = models.TextField()
+    isCompleted = models.BooleanField(default = False)
     time = models.TimeField(auto_now= True)
